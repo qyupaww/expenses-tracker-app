7db8d43fb416cb97c2a13a4ed7a342ef9af46bdd
Faisal Rahman <qyupaww@gmail.com>
2025-12-01 22:22:34 +0700

    fix column

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import '../model/expense.dart';
+import 'expenses_list.dart';
+
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
 
@@ -8,13 +11,26 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        title: 'Flutter Course',
+        amount: 109000,
+        date: DateTime.now(),
+        category: Category.others),
+    Expense(
+        title: 'Nasi Padang',
+        amount: 10000,
+        date: DateTime.now(),
+        category: Category.food),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
         children: [
-          Text("The Chart"),
-          Text("Expenses Lists"),
+          const Text("The Chart"),
+          Expanded(child: ExpensesList(expenses: _registeredExpenses)),
         ],
       ),
     );
